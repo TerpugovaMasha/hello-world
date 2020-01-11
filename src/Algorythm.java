9dde0691c3b68ae5ad770387ae83dca1cb9c47b7
TerpugovaMasha <56717263+TerpugovaMasha@users.noreply.github.com>
2020-01-11 13:51:00 +0600

    Update Algorythm.java

--- src/Algorythm.java
+++ src/Algorythm.java
@@ -1,7 +1,10 @@
 class Algorythm {
-    String msg;
+    private String msg;
     public Algorythm(){
-        msg = "Good Morning!";
+        msg = "Hello World!";
+    }
+	public Algorythm(String msg){
+        this.msg = msg;
     }
     public  void out() {
         System.out.println(msg);
